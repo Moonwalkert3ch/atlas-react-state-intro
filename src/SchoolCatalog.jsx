4600c9acd410a9c6bfaf1966579cbce236c77954
Moonwalkert3ch <6620@holbertonstudents.com>
2024-08-31 18:13:30 -0500

    sorted data display by clicking on the column header into ascending and descending order

--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from 'react';
 export default function SchoolCatalog() {
   const [courses, setCourses] = useState([]);
   const [searchInput, setSearchInput] = useState('');
+  const [sortColumn, setSortColumn] = useState('');
+  const [sortDirection, setSortDirection] = useState('asc');
+
 
 
   useEffect(() => {
@@ -18,6 +21,35 @@ export default function SchoolCatalog() {
     course.courseName.toLowerCase().includes(searchInput.toLowerCase())
     );
 
+  // sorting courses by colum and ascend and descend onclinck
+  const sortedCourses = [...filteredCourses].sort((a, b) => {
+    if (sortColumn) {
+      const firstValue = a[sortColumn];
+      const secondValue = b[sortColumn];
+
+      if (typeof firstValue === 'string') {
+        // sort strings
+        return sortDirection === 'asc'
+          ? firstValue.localeCompare(secondValue)
+          : secondValue.localeCompare(firstValue);
+      } else {
+        // sort numbers
+        return sortDirection === 'asc' ? firstValue - secondValue : secondValue - firstValue;
+      }
+    }
+    return 0; // makes sure no sorting if not clicked on column
+  });
+
+  // column header click handler
+  const handleSort = (column) => {
+    if (sortColumn === column) {
+      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortColumn(column);
+      setSortDirection('asc');
+    }
+  };
+
     return (
       <div className="school-catalog">
       <h1>School Catalog</h1>
@@ -30,16 +62,16 @@ export default function SchoolCatalog() {
       <table>
         <thead>
           <tr>
-            <th>Trimester</th>
-            <th>Course Number</th>
-            <th>Courses Name</th>
-            <th>Semester Credits</th>
-            <th>Total Clock Hours</th>
+            <th onClick={() =>handleSort('trimester')}>Trimester</th>
+            <th onClick={() =>handleSort('courseNumber')}>Course Number</th>
+            <th onClick={() =>handleSort('courseName')}>Courses Name</th>
+            <th onClick={() =>handleSort('semesterCredits')}>Semester Credits</th>
+            <th onClick={() =>handleSort('totalClockHours')}>Total Clock Hours</th>
             <th>Enroll</th>
           </tr>
         </thead>
         <tbody>
-          {filteredCourses.map((course, index) => (
+          {sortedCourses.map((course, index) => (
             <tr key={index}>
               <td>{course.trimester}</td>
               <td>{course.courseNumber}</td>
